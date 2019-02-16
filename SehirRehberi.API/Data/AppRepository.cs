407e01be658c6a89daa4709f6562c0c17e1595fd
Turan Yunus Çelikel <turanyunus34@gmail.com>
2019-02-16 18:47:53 +0300

    project added in git

---
+++ SehirRehberi.API/Data/AppRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Data
+{
+    public class AppRepository : IAppRepository
+    {
+        private DataContext _context;
+
+        public AppRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public List<City> GetCities()
+        {
+            var cities = _context.Cities.Include(c => c.Photos).ToList();
+            return cities;
+        }
+
+        public City GetCityById(int cityId)
+        {
+            var city = _context.Cities.Include(c => c.Photos).FirstOrDefault(c => c.Id == cityId);
+            return city;
+        }
+
+        public Photo GetPhoto(int id)
+        {
+            var photos = _context.Photos.FirstOrDefault(p => p.Id == id);
+            return photos;
+        }
+
+        public List<Photo> GetPhotosByCity(int Id)
+        {
+            var photo = _context.Photos.Where(p => p.CityId == Id).ToList();
+            return photo;  
+        }
+
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ SehirRehberi.API/Data/AuthRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private DataContext _context;
+
+        public AuthRepository(DataContext dataContext) {
+            _context = dataContext;
+        }
+
+        public async Task<User> Login(string userName, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == userName);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computeHash.Length; i++)
+                {
+                    if (computeHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, PasswordSalt;
+            CreatePasswordHash(password, out passwordHash, out PasswordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = PasswordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac=new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string userName)
+        {
+            if (await _context.Users.AllAsync(x=>x.Username == userName))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
